481c423086dc11cbbf3c898e04ba4cd332c2e23b
Rosy <rosydhakal333@gmail.com>
2024-02-13 13:25:21 +1100

    added on main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { SnackbarProvider } from 'notistack'
+// import store from './redux/store.js'
+import { BrowserRouter } from 'react-router-dom'
+import { store } from './redux/store.js'
+// import { PersistGate } from 'redux-persist/integration/react'
+// import persistStore from 'redux-persist/es/persistStore'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      {/* <PersistGate loading={null} persistor={persistStore}> */}
+      <Provider store={store}>
+        <SnackbarProvider>
+          <App />
+        </SnackbarProvider>
+      </Provider>
+      {/* </PersistGate> */}
+    </BrowserRouter>
+  </React.StrictMode>
 )
