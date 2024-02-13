a1d9bc7c933f22ab733aa3d428cc447ec76a31d3
Rosy <rosydhakal333@gmail.com>
2024-02-13 13:30:00 +1100

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import Login from '../admin/auth/Login'
-import Signup from '../admin/auth/Signup'
-import Dashboard from '../admin/components/Dashboard'
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Login from './admin/auth/Login'
+import SignUp from './admin/auth/SignUp'
+import Dashboard from './admin/components/Dashboard'
+import ResetPassword from './admin/auth/ResetPassword'
 
 function App() {
   return (
     <>
-    <Login />
-    <Signup />
-    <Dashboard />
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/reset-password" element={<ResetPassword />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
     </>
   )
 }
