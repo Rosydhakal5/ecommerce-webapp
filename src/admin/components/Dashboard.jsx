4d4f0839cd930dcc2b0dbb50e88fdb5b5fd1823c
Rosy <rosydhakal333@gmail.com>
2024-02-16 01:41:46 +1100

    changed Header, Footer and Navigation menu

--- src/admin/components/Dashboard.jsx
+++ src/admin/components/Dashboard.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React from 'react'
+import BaseLayout from '../pageLayout/BaseLayout'
 
-const Dashboard = () =>{
-    return(
-        <>
-        this is dashboard
-        </>
-
-    )
+const Dashboard = () => {
+  return (
+    <>
+      <BaseLayout>
+        <h1> this is dashbooard </h1>
+      </BaseLayout>
+    </>
+  )
 }
-
 export default Dashboard
